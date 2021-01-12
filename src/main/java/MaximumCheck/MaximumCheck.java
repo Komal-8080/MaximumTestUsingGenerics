d0b975d645c43ab41939315f240348343ce3b831
Komal-8080 <shindeg163@gmail.com>
2021-01-12 19:36:17 +0530

    Added: Method to find maximum of three string values

--- src/main/java/MaximumCheck/MaximumCheck.java
+++ src/main/java/MaximumCheck/MaximumCheck.java
@@ -1,15 +1,13 @@
 package MaximumCheck;
 
-import java.awt.print.Printable;
-
 public class MaximumCheck {
 
-	//UC1: Method to check Maximum value from 3 Floating Values
-	public Float maximumValue(Float x, Float y, Float z) {
-		Float max = x;
-		if (y.compareTo(max) > 0) {
+	//UC1: Method to check Maximum value from 3 Strings
+	public String maximumValue(String x, String y, String z) {
+		String max = x;
+		if (y.compareToIgnoreCase(max) > 0) {
 			max = y;
-		} if (z.compareTo(max) > 0) {
+		} if (z.compareToIgnoreCase(max) > 0) {
 			max = z;
 		} 
 		System.out.println(max +" is Maximum");
--- src/test/java/MaximumCheck/MaximumCheckTest.java
+++ src/test/java/MaximumCheck/MaximumCheckTest.java
@@ -9,23 +9,23 @@ public class MaximumCheckTest {
 	@Test
 	public void MaximumValueAtFirstPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		float maximum = maximumCheck.maximumValue(15.2f, 12.4f, 14.7f);
-		Assert.assertEquals(15.2, maximum, 0.001);
+		String maximum = maximumCheck.maximumValue("banana", "kiwi", "apple");
+		Assert.assertEquals("kiwi",maximum);
 	}
 
 	// test to check if maximum value is at second position.
 	@Test
 	public void MaximumValueAtSecondPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		float maximum = maximumCheck.maximumValue(12.45f, 15.2f, 14.7f);
-		Assert.assertEquals(15.2, maximum, 0.001);
+		String maximum = maximumCheck.maximumValue("kiwi", "banana", "apple");
+		Assert.assertEquals("kiwi",maximum);
 	}
 
 	// test to check if maximum value is at third position.
 	@Test
 	public void MaximumValueAtThirdPosition() {
 		MaximumCheck maximumCheck = new MaximumCheck();
-		float maximum = maximumCheck.maximumValue(12.45f, 14.7f, 15.2f);
-		Assert.assertEquals(15.2, maximum, 0.001);
+		String maximum = maximumCheck.maximumValue("apple", "kiwi", "bananan");
+		Assert.assertEquals("kiwi",maximum);
 	}
 }
